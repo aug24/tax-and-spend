235d001d9d16b28c0f1964c42efd1048360ac321
aug24 <justin.rowles@gmail.com>
2020-09-16 17:53:13 +0100

    Add basic data

---
+++ src/data/elections.js
@@ -0,0 +1,10 @@
+export const elections = {
+"5 July 1945":	"L",
+"25 October 1951":	"C",
+"5 October 1964":	"L",
+"18 June 1970":	"C",
+"28 February 1974":	"L",
+"3 May 1979":	"C",
+"1 May 1997":	"L",
+"6 May 2010":	"C"
+}
